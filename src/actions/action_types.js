d4d9085caa5453c872ae6035424042ee1059e4e7
haegyun <haegyun@jeonghaegyuns-MacBook-Pro.local>
2017-04-11 17:23:55 +0900

    비동기 통신 액션 생성자 정의

--- src/actions/action_types.js
+++ src/actions/action_types.js
@@ -1,3 +1,13 @@
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+// 비동기통신 액션타입
+// 표시할 reddit을 선택할 때
+export const SELECT_REDDIT = 'SELECT_REDDIT';
+// 새로고침할 때
+export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT';
+// 포스트를 요청할 때
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+// 포스트가 있는 응답이 도착했을 때
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,25 +5,55 @@ export const addTodo = function(text, completed) {
         type: types.ADD_TODO,
         text,
         completed
-    }
+    };
 };
 
 export const toggleTodo = function(index) {
     return {
         type: types.TOGGLE_TODO,
         index
-    }
+    };
 };
 
 export const setVisibilityFilter = function(filter) {
     return {
         type: types.SET_VISIBILITY_FILTER,
         filter
-    }
+    };
 };
 
 export const VISIBILITY_FILTERS = {
     SHOW_ALL: 'all',
     SHOW_ACTIVE: 'active',
     SHOW_COMPLETED: 'completed'
+};
+
+export const selectReddit = function(reddit) {
+    return {
+        type: types.SELECT_REDDIT,
+        reddit
+    };
+};
+
+export const invalidateReddit = function(reddit) {
+    return {
+        type: types.INVALIDATE_REDDIT,
+        reddit
+    };
+};
+
+export const requestPosts = function(reddit) {
+    return {
+        type: types.REQUEST_POSTS,
+        reddit
+    };
+};
+
+export const receivePosts = function(reddit, json) {
+    return {
+        type: types.RECEIVE_POSTS,
+        reddit,
+        posts: json.data.children.map(child => child.data),
+        receiveAt: Date.now()
+    };
 };
