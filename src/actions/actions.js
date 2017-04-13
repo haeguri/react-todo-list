01276513a45c7955c0cd6fd0312a6d1b7b2c91df
haegyun <haegyun@jeonghaegyuns-MacBook-Pro.local>
2017-04-13 18:51:58 +0900

    비동기 액션 생성자 제거

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -27,33 +27,3 @@ export const VISIBILITY_FILTERS = {
     SHOW_ACTIVE: 'active',
     SHOW_COMPLETED: 'completed'
 };
-
-export const selectReddit = function(reddit) {
-    return {
-        type: types.SELECT_REDDIT,
-        reddit
-    };
-};
-
-export const invalidateReddit = function(reddit) {
-    return {
-        type: types.INVALIDATE_REDDIT,
-        reddit
-    };
-};
-
-export const requestPosts = function(reddit) {
-    return {
-        type: types.REQUEST_POSTS,
-        reddit
-    };
-};
-
-export const receivePosts = function(reddit, json) {
-    return {
-        type: types.RECEIVE_POSTS,
-        reddit,
-        posts: json.data.children.map(child => child.data),
-        receiveAt: Date.now()
-    };
-};
